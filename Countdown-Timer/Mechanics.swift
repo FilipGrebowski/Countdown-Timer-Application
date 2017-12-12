d9a99c5741eb13d017e977f80b797d1a00a6e4d8
Filip Grebowski <grebowskifilip@gmail.com>
2017-12-12 14:21:12 +0000

    Updated project 2/3

--- Countdown-Timer/Mechanics.swift
+++ Countdown-Timer/Mechanics.swift
@@ -10,15 +10,50 @@ import UIKit
 
 class Mechanics: UIViewController {
     
+    var timer = Timer()
     
+    var timeHour = timePicker.GlobalVariable.extracted_hour
+    var timeMinute = timePicker.GlobalVariable.extracted_minute
+    var timeSecond = timePicker.GlobalVariable.extracted_second
     
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var minutes: UILabel!
+    @IBOutlet weak var seconds: UILabel!
     
-    
-    
-    
+    @IBAction func timerStart() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Mechanics.processTimer), userInfo: nil, repeats: true)
+    }
 
+    @objc func processTimer() {
+        if timeSecond > 0 {
+            seconds?.text = String(timeSecond)
+            timeSecond = timeSecond - 1
+        }
+        else if timeSecond == 0 {
+            seconds?.text = "59"
+            timeMinute = timeMinute - 1
+            minutes?.text = String(timeMinute)
+        }
+        else {
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func timerStop(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        timePicker.GlobalVariable.extracted_hour = 0
+        timePicker.GlobalVariable.extracted_minute = 0
+        timePicker.GlobalVariable.extracted_second = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        hours?.text = String(timeHour)
+        minutes?.text = String(timeMinute)
+        seconds?.text = String(timeSecond)
     }
 
     override func didReceiveMemoryWarning() {
--- Countdown-Timer/timePicker.swift
+++ Countdown-Timer/timePicker.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let hour = Array(0...23)
@@ -18,6 +19,34 @@ class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     var recievedString: String = ""
     
     @IBOutlet weak var timepicker: UIPickerView!
+    @IBOutlet weak var acceptButton: UIButton!
+    @IBOutlet weak var acceptButtonOrange: UIButton!
+    
+    struct GlobalVariable {
+        static var extracted_hour = Int()
+        static var extracted_minute = Int()
+        static var extracted_second = Int()
+    }
+
+    // Resets the timepicker hour, minute and second values to 0.
+    @IBAction func reset(_ sender: Any) {
+        timepicker.selectRow(0, inComponent: 0, animated: true)
+        timepicker.selectRow(0, inComponent: 2, animated: true)
+        timepicker.selectRow(0, inComponent: 4, animated: true)
+    }
+    
+    // Confirms the time and moves on to second view, unless user has not
+    // selected a time. In that case an alert message pops up.
+    @IBAction func accept(_ sender: Any) {
+        if GlobalVariable.extracted_hour == 0 && GlobalVariable.extracted_minute == 0 && GlobalVariable.extracted_second == 0 {
+            
+            let alertController = UIAlertController(title: "Unable to Proceed", message:
+                "Please select a time", preferredStyle: UIAlertControllerStyle.actionSheet)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
     
     // Returns the number of columns in the picker view.
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -45,20 +74,22 @@ class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         }
     }
     
-    
     // Sets the current picker value to a label.
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//
-//        if component == 0 {
-//            labelHour.text = String(hour[row])
-//        }
-//        else if component == 2 {
-//            labelMinute.text = String(minute[row])
-//        }
-//        else if component == 4 {
-//            labelSecond.text = String(second[row])
-//        }
-//    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        switch component {
+            case 0:
+                GlobalVariable.extracted_hour = Int(hour[row])
+                break
+
+            case 2:
+                GlobalVariable.extracted_minute = Int(minute[row])
+                break
+
+            default:
+                GlobalVariable.extracted_second = Int(second[row])
+        }
+    }
     
     // Sets all the specific attributes of UI picker view.
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
@@ -68,20 +99,25 @@ class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         pickerLabel.textAlignment = .center
         pickerLabel.font = UIFont(name: "AlexandriaFLF", size: 80)
         
-        if component == 0 {
-            pickerLabel.text = String(hour[row])
-        }
-        else if component == 1 {
-            pickerLabel.text = ":"
-        }
-        else if component == 2 {
-            pickerLabel.text = String(minute[row])
-        }
-        else if component == 3 {
-            pickerLabel.text = ":"
-        }
-        else {
-            pickerLabel.text = String(second[row])
+        switch component {
+            case 0:
+                pickerLabel.text = String(hour[row])
+                break
+            
+            case 1:
+                pickerLabel.text = ":"
+                break
+            
+            case 2:
+                pickerLabel.text = String(minute[row])
+                break
+            
+            case 3:
+                pickerLabel.text = ":"
+                break
+            
+            default:
+                pickerLabel.text = String(second[row])
         }
         
         return pickerLabel
@@ -93,6 +129,7 @@ class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         return 80
     }
     
+    // Sets the width for each column.
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         
         if component == 0 {
@@ -114,8 +151,10 @@ class timePicker: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Upon loading the app, set the defualt value to 30 seconds.
-        timepicker.selectRow(30, inComponent: 2, animated: true)
+        timepicker.alpha = 0
+        UIView.animate(withDuration: 1, animations: {
+            self.timepicker.alpha = 1
+        })
     }
 
     override func didReceiveMemoryWarning() {
